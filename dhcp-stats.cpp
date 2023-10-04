829b1b1ca798e49b77aa8bd3638ecae99e074b26
Patrik Potancok <xpotan00@vutbr.cz>
2023-10-04 15:06:31 +0200

    feat: checking if the interface on inpput is valid

--- dhcp-stats.cpp
+++ dhcp-stats.cpp
@@ -5,10 +5,19 @@
 #include <iostream>
 #include <signal.h>
 #include <getopt.h>
+#include <signal.h>
+
+// Handling CTRL + C
+void signal_handler(int signum){
+    (void)signum;
+    printf("Closing... \n");
+    //pcap_close(descr);
+    exit(0);
+}
 
 //testing commit
 
-int check_args(int argc, char **argv, std::string& pcapFile, std::vector<std::string>& interfaces) {
+int check_args(int argc, char **argv, std::string& pcapFile, std::string& interface, std::vector<std::string>& ip_prefixes) {
     int opt;
     
     while ((opt = getopt(argc, argv, "r:i:h")) != -1) {  
@@ -22,12 +31,17 @@ int check_args(int argc, char **argv, std::string& pcapFile, std::vector<std::st
                 break;
             case 'i':
                 if (optarg) {
+                    interface = std::string(optarg);
                     while (optind < argc && argv[optind][0] != '-') {
-                        interfaces.push_back(std::string(argv[optind]));
+                        ip_prefixes.push_back(std::string(argv[optind]));
                         optind++;
                     }
+                    if(ip_prefixes.empty()) {
+                        std::cerr << "Error: IP addresses must follow the interface argument." << std::endl;
+                        return -1;
+                    }
                 } else {
-                    return -1;  
+                    return -1;
                 }
                 break;
             case 'h':
@@ -37,7 +51,6 @@ int check_args(int argc, char **argv, std::string& pcapFile, std::vector<std::st
                 std::cout << "-h              : Show this help message." << std::endl;
                 exit(0);  // Exit the program after showing help
             default:
-                std::cerr << "Usage: " << argv[0] << " [-r <filename>] [-i <interface-name> ...] <ip-prefix> [<ip-prefix> ... ]" << std::endl;
                 return -1;  
         }
     }
@@ -47,18 +60,61 @@ int check_args(int argc, char **argv, std::string& pcapFile, std::vector<std::st
 
 int main (int argc, char ** argv) {
     std::string pcapFile;
-    std::vector<std::string> interfaces;
+    std::string interface;
+    std::vector<std::string> ip_prefixes;
 
-    int arguments = check_args(argc, argv, pcapFile, interfaces);
+    int arguments = check_args(argc, argv, pcapFile, interface, ip_prefixes);
 
     if (arguments != 0){
         std::cerr << "Usage: " << argv[0] << " [-r <filename>] [-i <interface-name> ...] <ip-prefix> [<ip-prefix> ... ]" << std::endl;
         return 0;
     }
      
-
-    for (const auto& interface : interfaces) {
-    std::cout << interface << std::endl;
+    std::cout << "Interface: " << interface << std::endl;
+    
+    for(const auto& ip : ip_prefixes) {
+        std::cout << ip << std::endl;
     }
- //commit check
+    
+    
+    pcap_if_t *alldevs;
+    //, *device;
+    //pcap_if_t *interface_select = NULL; 
+    //bpf_u_int32 pMask;
+    //bpf_u_int32 pNet;
+    //struct bpf_program fp;
+    //int count = 0;
+    char errbuf[PCAP_ERRBUF_SIZE];
+
+    // Handling CTRL + C
+    signal(SIGINT, signal_handler);
+
+    //this part is inspired by https://www.thegeekstuff.com/2012/10/packet-sniffing-using-libpcap/
+    if(pcap_findalldevs(&alldevs, errbuf) == PCAP_ERROR){
+        fprintf(stderr, "Error finding interface: %s\n", errbuf);
+        exit(1);
+    }
+    
+    //check if user selected a valid interface
+    bool isInterfaceValid = false;
+    for (pcap_if_t *d = alldevs; d; d = d->next) {
+        if (interface == d->name) {
+            isInterfaceValid = true;
+            break;
+        }
+    }
+    
+    //if no selected interface was valid print a list of all avaible interfaces
+    if (!isInterfaceValid) {
+        std::cerr << "Invalid interface: " << interface << std::endl;
+        std::cerr << "Available interfaces are: " << std::endl;
+
+        for (pcap_if_t *d = alldevs; d; d = d->next) {
+            std::cerr << d->name << std::endl;
+            }
+            pcap_freealldevs(alldevs);
+            return 1;
+    }
+
+    pcap_freealldevs(alldevs);
 }
