981f8fdd86d725b9371ad66a824b2d1c25122935
Patrik Potancok <xpotan00@vutbr.cz>
2023-10-04 16:39:16 +0200

    feat: checking if there is nothing on input

--- dhcp-stats.cpp
+++ dhcp-stats.cpp
@@ -51,7 +51,8 @@ int check_args(int argc, char **argv, std::string& pcapFile, std::string& interf
                 std::cout << "-h              : Show this help message." << std::endl;
                 exit(0);  // Exit the program after showing help
             default:
-                return -1;  
+                std::cerr << "Error: IP addresses must follow the interface argument." << std::endl;
+                return -1; 
         }
     }
     return 0;
@@ -62,12 +63,27 @@ int main (int argc, char ** argv) {
     std::string pcapFile;
     std::string interface;
     std::vector<std::string> ip_prefixes;
+    pcap_if_t *alldevs, *device;
+    pcap_if_t *interface_select = NULL; 
+    bpf_u_int32 pMask;
+    bpf_u_int32 pNet;
+    //struct bpf_program fp;
+    pcap_t *descr;
+    //int count = 0;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    int arguments = 0;
 
-    int arguments = check_args(argc, argv, pcapFile, interface, ip_prefixes);
-
-    if (arguments != 0){
+    if (argc > 1) {  
+        arguments = check_args(argc, argv, pcapFile, interface, ip_prefixes);
+    } else {
         std::cerr << "Usage: " << argv[0] << " [-r <filename>] [-i <interface-name> ...] <ip-prefix> [<ip-prefix> ... ]" << std::endl;
-        return 0;
+        exit(0);
+    }
+    
+
+    if (arguments != 0) {
+        std::cerr << "Usage: " << argv[0] << " [-r <filename>] [-i <interface-name> ...] <ip-prefix> [<ip-prefix> ... ]" << std::endl;
+        exit(0);
     }
      
     std::cout << "Interface: " << interface << std::endl;
@@ -75,30 +91,20 @@ int main (int argc, char ** argv) {
     for(const auto& ip : ip_prefixes) {
         std::cout << ip << std::endl;
     }
-    
-    
-    pcap_if_t *alldevs;
-    //, *device;
-    //pcap_if_t *interface_select = NULL; 
-    //bpf_u_int32 pMask;
-    //bpf_u_int32 pNet;
-    //struct bpf_program fp;
-    //int count = 0;
-    char errbuf[PCAP_ERRBUF_SIZE];
 
     // Handling CTRL + C
     signal(SIGINT, signal_handler);
 
     //this part is inspired by https://www.thegeekstuff.com/2012/10/packet-sniffing-using-libpcap/
-    if(pcap_findalldevs(&alldevs, errbuf) == PCAP_ERROR){
+    if (pcap_findalldevs(&alldevs, errbuf) == PCAP_ERROR) {
         fprintf(stderr, "Error finding interface: %s\n", errbuf);
         exit(1);
     }
     
     //check if user selected a valid interface
     bool isInterfaceValid = false;
-    for (pcap_if_t *d = alldevs; d; d = d->next) {
-        if (interface == d->name) {
+    for (device = alldevs; device; device = device->next) {
+        if (interface == device->name) {
             isInterfaceValid = true;
             break;
         }
@@ -109,11 +115,32 @@ int main (int argc, char ** argv) {
         std::cerr << "Invalid interface: " << interface << std::endl;
         std::cerr << "Available interfaces are: " << std::endl;
 
-        for (pcap_if_t *d = alldevs; d; d = d->next) {
-            std::cerr << d->name << std::endl;
+        for (device = alldevs; device; device = device->next) {
+            std::cerr << device->name << std::endl;
             }
             pcap_freealldevs(alldevs);
             return 1;
+    }
+
+
+    if (pcap_lookupnet(interface_select->name, &pNet, &pMask, errbuf) == PCAP_ERROR) {
+        pMask = 0;
+        pNet = 0;
+    }
+
+    descr = pcap_open_live(interface_select->name, BUFSIZ, 1, 1000, errbuf);
+    pcap_freealldevs(alldevs);
+
+    if (descr == NULL) {
+        pcap_close(descr);
+        fprintf(stderr, "pcap_open_live() failed due to [%s]\n", errbuf);
+        exit(1);
+    }
+
+    if (pcap_datalink(descr) != DLT_EN10MB) {
+        pcap_close(descr);
+        fprintf(stderr, "Device does not have ethernet headers\n");
+        exit(1);
     }
 
     pcap_freealldevs(alldevs);
